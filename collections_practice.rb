a2eb72638b17d657f7cc740e7726dfd162d66678
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-11-24 23:42:57 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
-# your code goes here
+def begins_with_r(array)
+  test = array.map { |word| word.start_with?('r') }
+  result = test.include?(false)
+  !result
+end
+
+def contain_a(array)
+  new_array = []
+  array.each { |word| new_array.push(word) if word.include?("a")}
+  new_array
+end
+
+def first_wa(array)
+  new_array = []
+  converted = array.map { |word| word.to_s }
+  converted.each { |word| new_array.push(word) if word.start_with?("wa")}
+  new_array[0]
+end
+
+def remove_non_strings(array)
+  new_array = []
+  array.each { |elem| new_array.push(elem) if elem.class == String }
+  new_array
+end
+
+def count_elements(array)
+  i = 0
+  unique = array.uniq
+  while i < unique.length
+    matches = array.select {|x| x == unique[i]}
+    unique[i][:count] = matches.length
+    i += 1
+  end
+  unique
+end
+
+def merge_data(keys, data)
+  merged = []
+  
+end
+
+def find_cool(temp)
+  temp.select {|elem| elem[:temperature] == "cool" }
+end
